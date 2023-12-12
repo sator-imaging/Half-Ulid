eb829a012dda579c5bb557f699f876e76de2b060
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2023-12-12 21:39:42 +0900

    add: template

---
+++ .docfx/template/public/main.js
@@ -0,0 +1,16 @@
+export default {
+    defaultTheme: 'light',
+    iconLinks: [
+      {
+        icon: 'github',
+        href: 'https://github.com/sator-imaging/Half-Ulid',
+        title: 'GitHub'
+      },
+      {
+        icon: 'twitter',
+        href: 'https://twitter.com/sator_imaging',
+        title: 'Twitter'
+      }
+    ]
+  }
+  
