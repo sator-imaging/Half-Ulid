02fbb1eca0c2a9389a13cddff6326b17f1641741
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2023-12-12 22:22:54 +0900

    upd

--- .docfx/template/public/main.js
+++ .docfx/template/public/main.js
@@ -3,13 +3,18 @@ export default {
   iconLinks: [
     {
       icon: 'github',
-      href: 'https://github.com/sator-imaging/Half-Ulid',
+      href: 'https://github.com/sator-imaging',
       title: 'GitHub'
     },
     {
       icon: 'twitter',
       href: 'https://twitter.com/sator_imaging',
       title: 'Twitter'
-    }
+    },
+    {
+      icon: 'youtube',
+      href: 'https://www.youtube.com/@SatorImaging',
+      title: 'YouTube'
+    },
   ]
 }
