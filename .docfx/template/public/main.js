c760a4f325abfa576e0d0cfbf61747a488df9086
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2023-12-12 21:45:40 +0900

    upd

--- .docfx/template/public/main.js
+++ .docfx/template/public/main.js
@@ -1,16 +1,15 @@
 export default {
-    defaultTheme: 'light',
-    iconLinks: [
-      {
-        icon: 'github',
-        href: 'https://github.com/sator-imaging/Half-Ulid',
-        title: 'GitHub'
-      },
-      {
-        icon: 'twitter',
-        href: 'https://twitter.com/sator_imaging',
-        title: 'Twitter'
-      }
-    ]
-  }
-  
+  defaultTheme: 'light',
+  iconLinks: [
+    {
+      icon: 'github',
+      href: 'https://github.com/sator-imaging/Half-Ulid',
+      title: 'GitHub'
+    },
+    {
+      icon: 'twitter',
+      href: 'https://twitter.com/sator_imaging',
+      title: 'Twitter'
+    }
+  ]
+}
