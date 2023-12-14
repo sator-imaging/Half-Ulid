c4988de4464e78b8043e1154e95a6b272b7c4e7a
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2023-12-14 11:17:27 +0900

    docfx

--- .docfx/template/public/main.js
+++ .docfx/template/public/main.js
@@ -1,25 +1,25 @@
 export default {
-  defaultTheme: 'light',
-  iconLinks: [
-    {
-      icon: 'github',
-      href: 'https://github.com/sator-imaging',
-      title: 'GitHub'
-    },
-    {
-      icon: 'twitter',
-      href: 'https://twitter.com/sator_imaging',
-      title: 'Twitter'
-    },
-    {
-      icon: 'youtube',
-      href: 'https://www.youtube.com/@SatorImaging',
-      title: 'YouTube'
-    },
-    {
-      icon: 'chat-quote-fill',
-      href: 'https://www.sator-imaging.com/',
-      title: 'Contact'
-    },
-  ]
+    defaultTheme: 'light',
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/sator-imaging',
+            title: 'GitHub'
+        },
+        {
+            icon: 'twitter',
+            href: 'https://twitter.com/sator_imaging',
+            title: 'Twitter'
+        },
+        {
+            icon: 'youtube',
+            href: 'https://www.youtube.com/@SatorImaging',
+            title: 'YouTube'
+        },
+        {
+            icon: 'chat-quote-fill',
+            href: 'https://www.sator-imaging.com/',
+            title: 'Contact'
+        },
+    ]
 }
